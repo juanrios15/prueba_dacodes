eb5d752c28c06a2d6618cd457d3b3c99a36e0b1a
juan <juankrios15@gmail.com>
2025-12-13 21:18:41 -0500

    Deleted unnecessary comments

--- app/domain/helpers/rounding.py
+++ app/domain/helpers/rounding.py
@@ -8,8 +8,3 @@ getcontext().rounding = ROUND_HALF_EVEN
 
 def round_amount(value) -> float:
     return float(round(Decimal(str(value)), 2))
-
-
-# TODO: Aplicar esto en donde se hagan calculos de redondeo
-# TODO: Agregar tests para redondeo half even
-# TODO: Crear README.md
